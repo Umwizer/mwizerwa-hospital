813d3c5f57105ce4189d6ecbbd3f5b2447c0dd0c
Umwizer <ruthumwizerwa@gmail.com>
2026-08-21 13:36:48 +0200

    adding images

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text className="text-2xl text-blue-600 font-bold">Mwizerwa Hospital</Text>
+    </View>
+  );
+}
